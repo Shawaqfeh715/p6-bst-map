1243881daff5eac976c932ceaa0589c9231087dd
Shawaqfeh715 <Anasshaw@umich.edu>
2026-04-18 12:48:29 -0400

    added string and sstream

---
+++ BinarySearchTree_tests.cpp
@@ -0,0 +1,11 @@
+#include "BinarySearchTree.hpp"
+#include "unit_test_framework.hpp"
+#include <string>
+#include <sstream>
+
+
+TEST(test_empty_construction){
+    BinarySearchTree<int>tree;
+    
+}
+TEST_MAIN()
